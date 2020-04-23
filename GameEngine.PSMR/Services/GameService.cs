f63ba8475013b614be69e8c7719ff8f25f8f42c9
unknown <lucie_hammond@hotmail.fr>
2020-04-23 23:50:46 +0200

    Add models and interfaces for defining Services

---
+++ GameEngine.PSMR/Services/GameService.cs
@@ -0,0 +1,12 @@
+﻿using GameEngine.PSMR.Rules;
+
+namespace GameEngine.PSMR.Services
+{
+    /// <summary>
+    /// Abstract template representing a Game Service. Each custom service in a project must inherit from this class (or from a derivative)
+    /// In fact a GameService is just a GameRule, the difference is essentially motivated by organizational needs
+    /// </summary>
+    public abstract class GameService : GameRule
+    {
+    }
+}
