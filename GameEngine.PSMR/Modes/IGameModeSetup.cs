e5c5a3a6a029713ac3d9cab32026db27655ead80
unknown <lucie_hammond@hotmail.fr>
2020-04-23 22:44:43 +0200

    Add models and interfaces for defining GameMode setups

---
+++ GameEngine.PSMR/Modes/IGameModeSetup.cs
@@ -0,0 +1,56 @@
+﻿using GameEngine.PSMR.Modes.Policies;
+using GameEngine.PSMR.Rules;
+using GameEngine.PSMR.Rules.Scheduling;
+using System;
+using System.Collections.Generic;
+
+namespace GameEngine.PSMR.Modes
+{
+    /// <summary>
+    /// A setup model in the form of an interface to be implemented for defining the characteristics of a custom GameMode
+    /// </summary>
+    public interface IGameModeSetup
+    {
+        /// <summary>
+        /// The name of the GameMode
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// The ServiceSetup that the GameMode requires (its rules can have dependencies to it)
+        /// Set null if no ServiceSetup is required
+        /// </summary>
+        Type RequiredServiceSetup { get; }
+
+        /// <summary>
+        /// Instantiate all the GameRules of the GameMode and add them to the given RulesDictionary
+        /// </summary>
+        /// <param name="rules">The dictionary storing all the GameRules used in the GameMode</param>
+        void SetRules(ref RulesDictionary rules);
+
+        /// <summary>
+        /// Set and return the order in which the GameRules must be initialized
+        /// By deduction, the unloading operations will be done in reverse order
+        /// </summary>
+        /// <returns>An ordered list of the GameRule types</returns>
+        List<Type> GetInitUnloadOrder();
+
+        /// <summary>
+        /// Set and return a schedule to follow for organizing the update of the GameRules
+        /// </summary>
+        /// <returns>A list of RuleScheduling objects, associating each rule type to a SchedulePattern</returns>
+        List<RuleScheduling> GetUpdateScheduler();
+
+        /// <summary>
+        /// Define a set of configurations concerning error handling in the GameMode
+        /// </summary>
+        /// <returns>An ErrorPolicy object</returns>
+        ErrorPolicy GetErrorPolicy();
+
+        /// <summary>
+        /// Define a set of configurations concerning performance management in the GameMode
+        /// </summary>
+        /// <returns>A PerformancePolicy object</returns>
+        PerformancePolicy GetPerformancePolicy();
+    }
+}
---
+++ GameEngine.PSMR/Modes/Policies/ErrorPolicy.cs
@@ -0,0 +1,29 @@
+﻿namespace GameEngine.PSMR.Modes.Policies
+{
+    /// <summary>
+    /// Set of GameMode configurations concerning exceptions and errors handling
+    /// </summary>
+    public class ErrorPolicy
+    {
+        /// <summary>
+        /// If true, nothing special is done when an exception arise from a GameRule
+        /// The exception is just catched and logged and the process continues as if nothing ever happened
+        /// </summary>
+        public bool IgnoreExceptions;
+
+        /// <summary>
+        /// Kind of action to take when an error (or unignored exception) is detected
+        /// </summary>
+        public OnErrorBehaviour ReactionOnError;
+
+        /// <summary>
+        /// Indicate if the unload method of a GameRule can be skipped in case an error occurs in it
+        /// </summary>
+        public bool SkipUnloadIfError;
+
+        /// <summary>
+        /// The GameMode to load instead of the current one if it needs to be unloaded due to errors
+        /// </summary>
+        public IGameModeSetup FallbackMode;
+    }
+}
