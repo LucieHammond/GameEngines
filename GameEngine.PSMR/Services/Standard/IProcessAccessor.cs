82b107c2d72ef2be0f1ded4ad6324e123ea30a39
unknown <lucie_hammond@hotmail.fr>
2020-04-24 02:09:22 +0200

    Create a standard service for accessng the gameprocess from any rule

---
+++ GameEngine.PSMR/Services/Standard/IProcessAccessor.cs
@@ -0,0 +1,12 @@
+﻿using GameEngine.PSMR.Process;
+
+namespace GameEngine.PSMR.Services.Standard
+{
+    /// <summary>
+    /// A dependency interface used to allow the GameRules to access to the GameProcess that is responsible for them
+    /// </summary>
+    internal interface IProcessAccessor
+    {
+        GameProcess GetCurrentProcess();
+    }
+}
