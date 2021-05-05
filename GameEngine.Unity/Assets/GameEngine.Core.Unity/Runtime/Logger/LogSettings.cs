56be82776520f9507a6b8415acbcdad52cbe3a28
unknown <lucie_hammond@hotmail.fr>
2021-05-06 01:01:57 +0200

    [Unity/Core] Improve Unity Logger: add documentation, support multithreading and minor cosmetics

--- GameEngine.Unity/Assets/GameEngine.Core.Unity/Runtime/Logger/LogSettings.cs
+++ GameEngine.Unity/Assets/GameEngine.Core.Unity/Runtime/Logger/LogSettings.cs
@@ -1,20 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using UnityEngine;
 
 namespace GameEngine.Core.Logger
 {
+    /// <summary>
+    /// Settings for the runtime logs, allowing customization of display and filtering
+    /// </summary>
     public class LogSettings : ScriptableObject
     {
+        /// <summary>
+        /// The name of the asset that should store the Log Settings
+        /// </summary>
+        public const string ASSET_NAME = "LogSettings";
+
+        /// <summary>
+        /// The minimal level of logs to display. All logs with an inferior level of importance will be ignored
+        /// </summary>
         public LogLevel MinLogLevel;
 
+        /// <summary>
+        /// Whether or not to filter the logs on their tags
+        /// </summary>
         public bool ActivateFiltering;
 
+        /// <summary>
+        /// The tags on which to filter the logs. Only the logs corresponding to the tags in the list are displayed
+        /// </summary>
         public HashSet<string> TagsFilter;
 
+        /// <summary>
+        /// The colors to use when displaying the specific associated tags
+        /// </summary>
         public Dictionary<string, Color> TagsColors;
 
+        /// <summary>
+        /// Initialize a new instance of LogSettings
+        /// </summary>
         public LogSettings()
         {
             MinLogLevel = LogLevel.Info;
@@ -23,24 +45,41 @@ namespace GameEngine.Core.Logger
             TagsColors = new Dictionary<string, Color>();
         }
 
+        /// <summary>
+        /// Get the log settings that have been defined in the project through a dedicated asset (or default if not defined)
+        /// </summary>
+        /// <returns>The requested instance of LogSettings</returns>
         public static LogSettings GetSettings()
         {
-            LogSettings settings = (LogSettings) Resources.Load("LogSettings");
+            LogSettings settings = Resources.Load<LogSettings>(ASSET_NAME);
             if (settings == null)
                 settings = CreateInstance<LogSettings>();
 
             return settings;
         }
 
+        /// <summary>
+        /// Get a color for a tag, chosen in a pseudo-random way (from the hash code of the tag) among a palette of possible colors
+        /// </summary>
+        /// <param name="tag">The tag for which to get a color</param>
+        /// <returns>The RGBA color to associate with the tag</returns>
         public static Color GetColorForTag(string tag)
         {
-            ColorUtility.TryParseHtmlString($"#{Convert.ToString(tag.GetHashCode(), 16)}", out Color color);
-            color.r = Mathf.Lerp(0, 0.9f, color.r);
-            color.g = Mathf.Lerp(0, 0.9f, color.g);
-            color.b = Mathf.Lerp(0, 0.9f, color.b);
-            color.a = 1;
+            try
+            {
+                ColorUtility.TryParseHtmlString($"#{Convert.ToString(tag.GetHashCode(), 16)}", out Color color);
+                color.r = Mathf.Lerp(0, 0.9f, color.r);
+                color.g = Mathf.Lerp(0, 0.9f, color.g);
+                color.b = Mathf.Lerp(0, 0.9f, color.b);
+                color.a = 1;
 
-            return color;
+                return color;
+            }
+            catch (UnityException)
+            {
+                System.Random rand = new System.Random(tag.GetHashCode());
+                return new Color((float)rand.NextDouble() * 0.9f, (float)rand.NextDouble() * 0.9f, (float)rand.NextDouble() * 0.9f);
+            }
         }
     }
 }
--- GameEngine.Unity/Assets/GameEngine.Core.Unity/Runtime/Logger/UnityLogger.cs
+++ GameEngine.Unity/Assets/GameEngine.Core.Unity/Runtime/Logger/UnityLogger.cs
@@ -1,39 +1,64 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 
 namespace GameEngine.Core.Logger
 {
+    /// <summary>
+    /// A predefined logger that logs messages in the Unity console window
+    /// </summary>
     public class UnityLogger : ILogger
     {
-        private readonly Dictionary<string, Color> m_TagsColors;
+        private readonly ConcurrentDictionary<string, Color> m_TagsColors;
 
+        /// <summary>
+        /// Initialize a new instance of UnityLogger
+        /// </summary>
+        /// <param name="tagsColors">The default colors to use when displaying the associated tags</param>
         public UnityLogger(Dictionary<string, Color> tagsColors = null)
         {
-            m_TagsColors = tagsColors ?? new Dictionary<string, Color>();
+            m_TagsColors = tagsColors != null ?
+                new ConcurrentDictionary<string, Color>(tagsColors) :
+                new ConcurrentDictionary<string, Color>();
         }
 
+        /// <summary>
+        /// <see cref="ILogger.LogDebug(string, string)"/>
+        /// </summary>
         public void LogDebug(string tag, string message)
         {
-            Debug.Log(FormatMessage(tag, message));
+            Debug.Log(FormatMessage(tag, message, true));
         }
 
+        /// <summary>
+        /// <see cref="ILogger.LogInfo(string, string)"/>
+        /// </summary>
         public void LogInfo(string tag, string message)
         {
             Debug.Log(FormatMessage(tag, message));
         }
 
+        /// <summary>
+        /// <see cref="ILogger.LogWarning(string, string)"/>
+        /// </summary>
         public void LogWarning(string tag, string message)
         {
             Debug.LogWarning(FormatMessage(tag, message));
         }
 
+        /// <summary>
+        /// <see cref="ILogger.LogError(string, string)"/>
+        /// </summary>
         public void LogError(string tag, string message)
         {
             Debug.LogError(FormatMessage(tag, message));
         }
 
+        /// <summary>
+        /// <see cref="ILogger.LogException(string, Exception)"/>
+        /// </summary>
         public void LogException(string tag, Exception e)
         {
             Exception firstException = e;
@@ -42,14 +67,16 @@ namespace GameEngine.Core.Logger
 
             FieldInfo messageField = firstException.GetType().GetField("_message", BindingFlags.Instance | BindingFlags.NonPublic);
             messageField.SetValue(firstException, FormatMessage(tag, firstException.Message));
-            
+
             Debug.LogException(e);
         }
 
-        private string FormatMessage(string tag, string message)
+        private string FormatMessage(string tag, string message, bool isDebug = false)
         {
             Color color = GetOrAddColor(tag);
             tag = string.Format("<color=#{0:X2}{1:X2}{2:X2}><b>{3}</b></color>", (byte)(color.r * 255f), (byte)(color.g * 255f), (byte)(color.b * 255f), tag);
+            if (isDebug)
+                message = $"<color=#FBFB99>{message}</color>";
             return $"[{tag}] {message}";
         }
 
@@ -58,7 +85,7 @@ namespace GameEngine.Core.Logger
             if (!m_TagsColors.TryGetValue(tag, out Color color))
             {
                 color = LogSettings.GetColorForTag(tag);
-                m_TagsColors.Add(tag, color);
+                m_TagsColors.TryAdd(tag, color);
             }
 
             return color;
