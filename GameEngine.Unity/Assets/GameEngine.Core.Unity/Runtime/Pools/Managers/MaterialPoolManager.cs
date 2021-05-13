54ea5fecb00fa43ba43752aa3283c31036c817cf
unknown <lucie_hammond@hotmail.fr>
2021-05-13 16:55:23 +0200

    [Unity/Core] Extend the pool system to manage Material pools

---
+++ GameEngine.Unity/Assets/GameEngine.Core.Unity/Runtime/Pools/Managers/MaterialPoolManager.cs
@@ -0,0 +1,22 @@
+﻿using GameEngine.Core.Pools.Descriptors;
+using GameEngine.Core.Pools.Poolers;
+using UnityEngine;
+
+namespace GameEngine.Core.Pools.Managers
+{
+    /// <summary>
+    /// A manager specialized in the processing of material pools
+    /// </summary>
+    public class MaterialPoolManager : PoolManager<Material, MaterialPooler, MaterialDescriptor>
+    {
+        /// <summary>
+        /// Instantiate a material pooler configured with the given material descriptor
+        /// </summary>
+        /// <param name="objectDescriptor">A descriptor characterizing the materials to pool with this pooler</param>
+        /// <returns>The created material pooler</returns>
+        protected override MaterialPooler CreateObjectPooler(MaterialDescriptor objectDescriptor)
+        {
+            return new MaterialPooler(objectDescriptor);
+        }
+    }
+}
