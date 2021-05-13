5ad0fcbabe0515ad7b9c6ecef50a04ba9b2f3087
unknown <lucie_hammond@hotmail.fr>
2021-05-13 16:53:27 +0200

    [Unity/Core] Extend the pool system to manage Texture2D pools

---
+++ GameEngine.Unity/Assets/GameEngine.Core.Unity/Runtime/Pools/Managers/TexturePoolManager.cs
@@ -0,0 +1,44 @@
+﻿using GameEngine.Core.Pools.Descriptors;
+using GameEngine.Core.Pools.Poolers;
+using UnityEngine;
+
+namespace GameEngine.Core.Pools.Managers
+{
+    /// <summary>
+    /// A manager specialized in the processing of texture2D pools
+    /// </summary>
+    public class TexturePoolManager : PoolManager<Texture2D, TexturePooler, TextureDescriptor>
+    {
+        /// <summary>
+        /// Instantiate a texture pooler configured with the given texture descriptor
+        /// </summary>
+        /// <param name="objectDescriptor">A descriptor characterizing the textures to pool with this pooler</param>
+        /// <returns>The created texture pooler</returns>
+        protected override TexturePooler CreateObjectPooler(TextureDescriptor objectDescriptor)
+        {
+            return new TexturePooler(objectDescriptor);
+        }
+
+        /// <summary>
+        /// Get a texture from the specified pool and load a given image into it
+        /// </summary>
+        /// <param name="poolId">The id of the pool</param>
+        /// <param name="imageData">The byte array containing the image data to load</param>
+        /// <param name="isRawData">
+        /// True if the raw image data have been preformatted for texture.
+        /// False (default) if the image data should be decoded from a commonly used format (png, jpeg, tga, tif)
+        /// </param>
+        /// <returns>The reserved texture with the image loaded</returns>
+        public Texture2D LoadImageOnPooledTexture(string poolId, byte[] imageData, bool isRawData = false)
+        {
+            Texture2D texture = GetObjectFromPool(poolId);
+            if (!isRawData)
+                texture.LoadImage(imageData);
+            else
+                texture.LoadRawTextureData(imageData);
+
+            texture.Apply(false, false);
+            return texture;
+        }
+    }
+}
---
+++ GameEngine.Unity/Assets/GameEngine.Core.Unity/Runtime/Pools/Poolers/TexturePooler.cs
@@ -0,0 +1,65 @@
+﻿using GameEngine.Core.Pools.Descriptors;
+using UnityEngine;
+
+namespace GameEngine.Core.Pools.Poolers
+{
+    /// <summary>
+    /// A pooling helper defining how to pool Unity 2D textures
+    /// </summary>
+    public class TexturePooler : IObjectPooler<Texture2D>
+    {
+        private TextureDescriptor m_TextureDescriptor;
+
+        /// <summary>
+        /// Initialize a new instance of TexturePooler
+        /// </summary>
+        /// <param name="descriptor">The descriptor containing configuration information for the pooled textures</param>
+        public TexturePooler(TextureDescriptor descriptor)
+        {
+            m_TextureDescriptor = descriptor;
+        }
+
+        /// <summary>
+        /// <see cref="IObjectPooler.CreateObject()"/>
+        /// </summary>
+        public Texture2D CreateObject()
+        {
+            Texture2D texture = new Texture2D(
+                m_TextureDescriptor.Width,
+                m_TextureDescriptor.Height,
+                m_TextureDescriptor.Format,
+                m_TextureDescriptor.UseMipMap,
+                m_TextureDescriptor.Linear);
+
+            texture.filterMode = m_TextureDescriptor.FilterMode;
+            texture.wrapMode = m_TextureDescriptor.WrapMode;
+            texture.anisoLevel = m_TextureDescriptor.AnisoLevel;
+
+            return texture;
+        }
+
+        /// <summary>
+        /// <see cref="IObjectPooler.PrepareObject(T)"/>
+        /// </summary>
+        public void PrepareObject(Texture2D pooledObject)
+        {
+
+        }
+
+        /// <summary>
+        /// <see cref="IObjectPooler.RestoreObject(T)"/>
+        /// </summary>
+        public void RestoreObject(Texture2D pooledObject)
+        {
+
+        }
+
+        /// <summary>
+        /// <see cref="IObjectPooler.DestroyObject(T)"/>
+        /// </summary>
+        public void DestroyObject(Texture2D pooledObject)
+        {
+            Object.Destroy(pooledObject);
+        }
+    }
+}
