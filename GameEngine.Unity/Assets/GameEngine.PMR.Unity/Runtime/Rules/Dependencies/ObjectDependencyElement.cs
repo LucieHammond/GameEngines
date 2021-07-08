bbe351bc2ec98db743f39723b3dd282414c68ae4
unknown <lucie_hammond@hotmail.fr>
2021-07-08 18:10:01 +0200

    [Unity/PMR] Create structure for GameObject dependencies in Unity

---
+++ GameEngine.Unity/Assets/GameEngine.PMR.Unity/Runtime/Rules/Dependencies/ObjectDependencyElement.cs
@@ -0,0 +1,23 @@
+﻿namespace GameEngine.PMR.Rules.Dependencies
+{
+    /// <summary>
+    /// The different parts of a gameobject that can be referenced in a rule as a dependency 
+    /// </summary>
+    public enum ObjectDependencyElement
+    {
+        /// <summary>
+        /// The gameobject itself
+        /// </summary>
+        GameObject,
+
+        /// <summary>
+        /// The transform of the gameobject
+        /// </summary>
+        Transform,
+
+        /// <summary>
+        /// A component of the gameobject
+        /// </summary>
+        Component
+    }
+}
