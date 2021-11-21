55c52b700d21d90f102d587df647b059cc0ed0ab
unknown <lucie_hammond@hotmail.fr>
2021-11-21 23:23:33 +0100

    [Core] Create a utility to set encoding formats from an enumeration

---
+++ GameEngine.Core/Utilities/Enums/EncodingType.cs
@@ -0,0 +1,33 @@
+﻿namespace GameEngine.Core.Utilities.Enums
+{
+    /// <summary>
+    /// Different types of character encoding formats for texts
+    /// </summary>
+    public enum EncodingType
+    {
+        /// <summary>
+        /// UTF-8 character format
+        /// </summary>
+        UTF8,
+
+        /// <summary>
+        /// UTF-7 character format
+        /// </summary>
+        UTF7,
+
+        /// <summary>
+        /// UTF-32 character format using the little endian byte order
+        /// </summary>
+        UTF32,
+
+        /// <summary>
+        /// UTF-16 character format using the little endian byte order
+        /// </summary>
+        UTF16,
+
+        /// <summary>
+        /// ASCII (7-bit) character format
+        /// </summary>
+        ASCII
+    }
+}
