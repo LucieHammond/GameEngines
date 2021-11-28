9ee25e6b5d8b7a8d1f7b4208f0c5078832bd3e36
unknown <lucie_hammond@hotmail.fr>
2021-11-28 15:56:48 +0100

    [UnityEditor] Create AssetBundleBuilder class, and associated settings

---
+++ GameEngine.Unity/Assets/GameEngine.Core.Unity/Editor/Build/AssetBundles/AssetBuildPlan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GameEngine.Core.UnityEditor.Build.AssetBundles
+{
+    /// <summary>
+    /// A custom definition of an asset bundle to create
+    /// </summary>
+    [Serializable]
+    public class AssetBuildPlan
+    {
+        /// <summary>
+        /// Create an instance of AssetBuildPlan
+        /// </summary>
+        public AssetBuildPlan()
+        {
+            BundleName = "NewBundle";
+            AssetNames = new string[0];
+        }
+
+        /// <summary>
+        /// The name of the asset bundle
+        /// </summary>
+        public string BundleName;
+
+        /// <summary>
+        /// The references of the assets to be included in the bundle (as paths in the Asset directory)
+        /// </summary>
+        public string[] AssetNames;
+    }
+}
---
+++ GameEngine.Unity/Assets/GameEngine.Core.Unity/Editor/Build/AssetBundles/AssetBuildScenario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEditor;
+
+namespace GameEngine.Core.UnityEditor.Build.AssetBundles
+{
+    /// <summary>
+    /// A scenario for creating and exporting asset bundles (targeting a particular platform for example)
+    /// </summary>
+    [Serializable]
+    public class AssetBuildScenario
+    {
+        /// <summary>
+        /// The name of the scenario (for Editor display)
+        /// </summary>
+        public string Name;
+
+        /// <summary>
+        /// If the scenario is currently activated for the next build
+        /// </summary>
+        public bool Activated;
+
+        /// <summary>
+        /// The path of the output directory where to export the asset bundles
+        /// </summary>
+        public string OutputPath;
+
+        /// <summary>
+        /// The asset bundle building options, used as flags
+        /// </summary>
+        public BuildAssetBundleOptions BuildOptions;
+
+        /// <summary>
+        /// The target platform for the build
+        /// </summary>
+        public BuildTarget TargetPlatform;
+    }
+}
